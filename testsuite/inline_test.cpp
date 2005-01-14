88149b7015a445de39d23559515b0a0362322343
jeanluc <jeanluc@d86972f7-00e6-0310-9e86-a100b9600b42>
2005-01-14 22:20:58 +0000

    A small test fil to verify inlining.  Type make inline_test to run the test.

---
+++ testsuite/inline_test.cpp
@@ -0,0 +1,38 @@
+#include <vector>
+#include <rodent/explicitrk.hpp>
+#include "simpharmonic.hpp"
+
+using namespace rodent;
+
+//
+// Test inlining of RHS function.
+//
+// Compile this program (to object with -c) with/without the -O flag.
+// Then run
+//
+//   objdump -Cd inline_test.o | grep SimpleHarmonic\<double\>::operator\(\)
+//
+// and see if the function is called or not.
+//
+
+std::vector<double> sho(double t)
+{
+  typedef SimpleHarmonic<double> Flow;
+  typedef AdaptiveRK4<Flow> Flow_int;
+
+  Flow flow(1.);
+  std::vector<double> y(flow.size());
+
+  double acc = 1.e-10;
+
+  // Initial conditions
+  y[0] = 0.;
+  y[1] = 1.;
+
+  Flow_int flow_int(flow);
+  flow_int
+    .tolerance(acc)
+    .setState(0.0,y);
+
+  return flow_int(t);
+}
