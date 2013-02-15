e6cfb443b8e9e14eec4e8bd7a9b3aea332a977f0
Jean-Luc Thiffeault <jeanluc@mailaps.org>
2013-02-15 06:53:35 -0600

    Added jlt_traits (from mixbyswim/code).

---
+++ rodent/jlt_traits.hpp
@@ -0,0 +1,28 @@
+#ifndef RODENT_JLT_TRAITS_HPP
+#define RODENT_JLT_TRAITS_HPP
+
+#include <jlt/math.hpp>
+#include <jlt/mathvector.hpp>
+#include <jlt/mathmatrix.hpp>
+#include <rodent/traits.hpp>
+
+
+namespace rodent {
+
+template<class float_type>
+struct vec_traits<jlt::mathvector<float_type> >
+{
+  typedef jlt::mathvector<float_type>				vec_type;
+  typedef typename jlt::mathvector<float_type>::value_type 	value_type;
+  typedef value_type						step_type;
+  typedef value_type						mag_type;
+  typedef jlt::mathvector<mag_type>				vec_mag_type;
+  typedef jlt::mathmatrix<value_type>				matrix_type;
+  static inline mag_type	absval(step_type _x) { return jlt::Abs(_x); }
+  static inline mag_type	mag(value_type _x) { return jlt::Abs(_x); }
+  static inline vec_type	copy(const vec_type& _v) { return _v; }
+};
+
+} // namespace rodent
+
+#endif // RODENT_JLT_TRAITS_HPP
